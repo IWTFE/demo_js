41b73938ada05426d272d9385311c517e43f1193
suxiaoyan <105807174@qq.com>
2017-02-24 09:26:53 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,61 @@
+<template>
+<div class='hello '>
+  <button @click='test'>发送请求</button>
+</div>
+</template>
+
+<script>
+import rest from 'src/js/security/rest_model'
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    test() {
+      var userId = '02 '
+      var loginPwd = '666 '
+      var shortSinglePhone = '02 '
+      var crmCorpInfoOid = '312312312gregre312312312312312 '
+
+      rest.post('http://192.168.88.56/user/login.do ', {
+        'body ': {
+          'userId ': userId,
+          'loginPwd ': loginPwd,
+          'shortSinglePhone ': shortSinglePhone,
+          'crmCorpInfoOid ': crmCorpInfoOid
+        },
+        'businessId ': 'CC_CRM_LOGIN '
+      }, function(res, code) {
+        console.log(res)
+      }, function(res) {
+        alert('网络连接问题，请稍后重试！ ')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/js/security/rest_modelnn.js
@@ -0,0 +1,21 @@
+  import { md5 }from './_md5.js'
+	import axios from '../../../node_modules/axios/dist/axios.min.js'
+	import CryptoJS from './mode-ecb-min.js'
+
+
+
+	/**
+	 *  POST请求
+	 */
+	Rest.prototype.post = function(url, param) {
+
+	};
+  function Rest() {
+    // ss
+  }
+  var rest = new Rest()
+  debugger
+  console.log(CryptoJS.aa)
+  console.log(CryptoJS.aa.enc)
+  CryptoJS.aa.enc.Utf8.parse('{f: f}');
+	export default rest
